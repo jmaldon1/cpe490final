6a54074491e7f2452306e55b03f88c59cc922968
Joshua <jmaldon1@stevens.edu>
2018-11-19 16:18:58 -0500

    added unique feature

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_socketio import SocketIO, send, emit
+from functools import wraps
 
 app = Flask(__name__)
 app.debug = True
@@ -7,29 +8,98 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
 
+# decorator that redirects logged in users
+def username_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'username' not in request.cookies:
+            return redirect(url_for('username'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+
 @app.route('/', methods=['GET', 'POST'])
 def username():
     if request.method == 'GET':
         return render_template('username.html')
-    return redirect(url_for('connect'))
+    return redirect(url_for('chat'))
 
 
-@app.route('/connect', methods=['GET'])
+@app.route('/chat', methods=['GET'])
+@username_required
+def chat():
+    # print(request.cookies)
+    return render_template('chat.html')
+
+
+# --- SOCKETIO ---
+
+# holds all users connected
+userList = {}
+
+
+# creates and empty user dict with the client ID as the key
+@socketio.on('connect')
 def connect():
-    return render_template('connect.html')
-
-# # listen for events on 'my event'
-# @socketio.on('my event')
-# def handle_my_custom_event(json):
-#     print('received json: ' + str(json))
-#     # send message to client
-#     emit('my response', 'You have succesfully contacted the server')
+    userList[request.sid] = {"username": '', "color": ''}
 
 
-@socketio.on('message')
+# adds a user to the dictionary that holds all users
+@socketio.on('setUser')
+def set_user(user):
+    userList[request.sid]['username'] = user
+
+
+# set users text color
+@socketio.on('setColor')
+def set_color(color):
+    userList[request.sid]['color'] = color
+
+
+# alerts everyone that a user connected
+@socketio.on('connectEvent')
+def connectEvent():
+    emit('messageEvent', '<li style="color:' + userList[request.sid]['color'] + ';">' +
+         userList[request.sid]['username'] + " has connected!" + '</li>', broadcast=True)
+
+
+# alerts everyone that a user disconnected
+@socketio.on('disconnect')
+def disconnect():
+    emit('messageEvent', '<li style="color:' + userList[request.sid]['color'] + ';">' +
+         userList[request.sid]['username'] + " has disconnected!" + '</li>', broadcast=True)
+    del userList[request.sid]
+
+
+# if message was successfully received by client, sent this to the original sender
+def ack(id):
+    emit('messageRecd', 'Message Sent', room=id)
+
+
+# all messages to client pass through here
+@socketio.on('messageEvent')
 def handleMessage(msg):
-    print('Message: ' + msg)
-    send(msg, broadcast=True)
+    # print(msg)
+    emit("messageEvent",
+         '<li style="color:' + userList[request.sid]['color'] + ';">' +
+         userList[request.sid]['username'] + ": " + msg + '</li>',
+         broadcast=True, callback=ack(request.sid))
+
+
+# all commands to client pass through here
+@socketio.on('commandEvent')
+def handleCommand(cmd):
+    # print(cmd)
+    emit("messageEvent", '<li style="color:' + userList[request.sid]['color'] + ';">' +
+        cmd['message'] + '</li>', broadcast=True)
+
+
+# all commands to client pass through here
+@socketio.on('invalidEvent')
+def invalidEvent(msg):
+    # print(cmd)
+    emit("messageEvent", '<li style="color:' + userList[request.sid]['color'] + ';">' +
+        msg + '</li>')
 
 
 if __name__ == '__main__':
---
+++ static/chat.js
@@ -0,0 +1,92 @@
+$(document).ready(function() {
+	var messageRecd = document.getElementById('messageReceived')
+	// get username from cookie
+	username = getCookie('username')
+
+	// connect a user
+	var socket = io.connect('http://localhost:5000');
+
+	// send details to server about the client that connected
+	socket.on('connect', function() {
+		socket.emit('setUser', username)
+		socket.emit('connectEvent');
+	});
+
+	// listens for disconnect button to be pressed
+	$('#disconnectButton').on('click', function() {
+		// disconnects a client
+		socket.close()
+	});
+
+	// when a user presses enter in the text box, the send button will be pressed
+	document.querySelector("#myMessage").addEventListener("keyup", event => {
+	    if(event.key !== "Enter") return; 
+	    document.querySelector("#sendButton").click();
+	    event.preventDefault();
+	});
+
+	// When user presses send button, send message to server
+	$('#sendButton').on('click', function() {
+		var messageStr = $('#myMessage').val()
+		if (messageStr.startsWith('/')){
+			var parsedMsg = messageStr.split(' ')
+			if(parsedMsg.length < 2 || parsedMsg[1] === '' || parsedMsg[1] === ' '){
+				socket.emit('invalidEvent', 'Please enter a value after the commands, example: /color blue')
+			}else if (parsedMsg[0] === '/nickname'){
+				socket.emit('setUser', parsedMsg[1])
+				socket.emit('commandEvent', {"message": username + ' changed their username to ' + parsedMsg[1]});
+				document.cookie = "username=" + parsedMsg[1];
+			} else if(parsedMsg[0] === '/color'){
+				socket.emit('setColor', parsedMsg[1])
+				socket.emit('commandEvent', {"message": username + ' changed their text color to ' + parsedMsg[1]});
+			} else{
+				console.log('invalid')
+				socket.emit('invalidEvent', parsedMsg[0] + ' is an invalid command');
+			}
+		}else{
+			socket.emit('messageEvent', messageStr);
+		}
+		// clear the input box everytime a message is sent
+		$('#myMessage').val('');
+	});
+
+	// when user disconnects, redirect them to username page
+	socket.on('disconnect', function(user){
+		window.location.href = '/';
+	})
+
+	// display messages on page
+	socket.on('messageEvent', function(msg) {
+		// console.log(msg)
+
+		// sticky scrollbox
+		var out = document.getElementById("out");
+		var isScrolledToBottom = out.scrollHeight - out.clientHeight <= out.scrollTop + 1;
+	    $("#messages").append(msg);
+	    if(isScrolledToBottom) out.scrollTop = out.scrollHeight - out.clientHeight;
+	});
+
+	// acknowledge that a message was recieved by the client
+	socket.on('messageRecd', function(msg){
+		messageRecd.innerHTML = msg
+		setTimeout(function() {
+		    messageRecd.innerHTML = '';
+		}, (3000));
+	});
+});
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
